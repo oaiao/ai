4bf26696e9e3dbd13f0f9a573456d75632321e2a
yuxiang.wang <yxuiang.wang@gmail.com>
2021-06-29 02:23:52 +0000

    Upgrade Python2  bugs

--- ai/11_03_Tensorflow/tf_utils.py
+++ ai/11_03_Tensorflow/tf_utils.py
@@ -66,7 +66,7 @@ def convert_to_one_hot(Y, C):
 
 
 def predict(X, parameters):
-    
+
     W1 = tf.convert_to_tensor(parameters["W1"])
     b1 = tf.convert_to_tensor(parameters["b1"])
     W2 = tf.convert_to_tensor(parameters["W2"])
@@ -81,12 +81,12 @@ def predict(X, parameters):
               "W3": W3,
               "b3": b3}
     
-    x = tf.placeholder(tf.float32, [12288, 1])
+    x = tf.compat.v1.placeholder(tf.float32, [12288, 1])
     
     z3 = forward_propagation_for_predict(x, params)
     p = tf.argmax(z3)
     
-    sess = tf.Session()
+    sess = tf.compat.v1.Session()
     prediction = sess.run(p, feed_dict = {x: X})
         
     return prediction
